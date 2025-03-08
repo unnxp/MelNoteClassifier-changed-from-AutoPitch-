21de66320186333a48b54f871ebebe6379754f07
Unnpxp_p <pram181147@gmail.com>
2025-03-08 13:47:52 +0700

    adding more requirement

---
+++ autopitchModel.py
@@ -0,0 +1,9 @@
+import librosa
+import librosa.display
+import numpy as np
+import json
+import os
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from keras import layers
